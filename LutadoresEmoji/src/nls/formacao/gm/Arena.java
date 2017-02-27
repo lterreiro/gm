47340c88166b9aefa8d835e8865f9d280780bfc0
nlsjavagm <luis.terreiro@nls.pt>
2017-02-27 21:30:43 +0000

    Add files via upload

--- LutadoresEmoji/src/nls/formacao/gm/Arena.java
+++ LutadoresEmoji/src/nls/formacao/gm/Arena.java
@@ -16,13 +16,13 @@ public class Arena {
      */
     public static void main(String[] args) {
         Lutador[] lutadores = new Lutador[6];
-        
-        lutadores[0] = new Lutador("Jolie Garçon", "França"   , 31, 1.75f,  68.9f,  11, 2, 1);
-        lutadores[1] = new Lutador("Putscript"   , "Brasil"   , 29, 1.68f,  57.8f,  14, 2, 3);
-        lutadores[2] = new Lutador("Snapshadow"  , "EUA"      , 35, 1.65f,  80.9f,  12, 2, 1);
-        lutadores[3] = new Lutador("Dead Code"   , "Australia", 28, 1.93f,  81.6f,  13, 0, 2);
-        lutadores[4] = new Lutador("Ufucobol"    , "Brasil"   , 37, 1.70f, 119.3f,   5, 4, 3);
-        lutadores[5] = new Lutador("Neerdaard"   , "EUA"      , 30, 1.81f, 105.7f,  12, 2, 4);
+
+        lutadores[0] = new Lutador("Jolie Garçon", "França", 31, 1.75f, 68.9f, 11, 2, 1);
+        lutadores[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
+        lutadores[2] = new Lutador("Snapshadow", "EUA", 35, 1.65f, 80.9f, 12, 2, 1);
+        lutadores[3] = new Lutador("Dead Code", "Australia", 28, 1.93f, 81.6f, 13, 0, 2);
+        lutadores[4] = new Lutador("Ufucobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
+        lutadores[5] = new Lutador("Neerdaard", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
         System.out.println("=========================================================");
         for (Lutador lutador : lutadores) {
             //lutador.apresentar();
@@ -30,20 +30,24 @@ public class Arena {
         }
         System.out.println("");
         System.out.println("");
-        Combate luta = new Combate() ;
-        luta.marcarCombate(lutadores[0], lutadores[1]);
-        System.out.println("");
-        System.out.println("===== Combate vai ser realizado com os seguintes lutadores ========");
-        luta.Combater();
-        
-      //lutadores[0].apresentar();
-       lutadores[0].status();
-        System.out.println("==============================");
-       //lutadores[1].apresentar();
-       lutadores[1].status();
-       System.out.println("==============================");
-        System.out.println("");
-       
+        Combate luta = new Combate();
+        int x = 2 ;
+        int y = 3 ;
+
+        luta.marcarCombate(lutadores[x], lutadores[y]);
+        if (luta.getAprovado()) {
+            System.out.println("");
+            System.out.println("===== Combate vai ser realizado com os seguintes lutadores ========");
+            luta.Combater();
+
+            //lutadores[0].apresentar();
+            lutadores[x].status();
+            System.out.println("==============================");
+            //lutadores[1].apresentar();
+            lutadores[y].status();
+            System.out.println("==============================");
+            System.out.println("");
+        }
     }
-    
+
 }
--- LutadoresEmoji/src/nls/formacao/gm/Combate.java
+++ LutadoresEmoji/src/nls/formacao/gm/Combate.java
@@ -12,56 +12,57 @@ import java.util.Random;
  * @author nls.formacao.gm
  */
 public class Combate {
+
     private Lutador adversario1;
     private Lutador adversario2;
     private int rounds;
     private boolean aprovado;
-    
-public void marcarCombate(Lutador lutador1 , Lutador lutador2) {
-    if (lutador1 != lutador2  && 
-            lutador1.getCategoria().equals(lutador2.getCategoria())) {
-        System.out.println("Combate pode ser marcado");
-        this.setAprovado(true);
-        this.setAdversario1(lutador1);
-        this.setAdversario2(lutador2);
-    } else {
-        this.setAprovado(false);
-        this.setAdversario1 = null;
-        this.setAdversario2 = null;
-        System.out.println("Combate nao pode ser marcado");
-    }
-}    
-    
-public void Combater() {
-    if (this.getAprovado()) {
-        this.adversario1.apresentar();
-        this.adversario2.apresentar();
-        
-        Random opcao = new Random() ;
-        int resultado = opcao.nextInt(3);
-        
-        System.out.println("========================================");
-        
-        switch(resultado) {
-            case 0:
-                System.out.println("Resultado do combate foi um empate !!");
-                this.adversario1.empatarLuta();
-                this.adversario2.empatarLuta();
-                break;
-            case 1:
-                System.out.println("O vencedor é " + this.adversario1.getNome());
-                this.adversario1.ganharLuta();
-                this.adversario2.perderLuta();
-                break;
-            case 2:
-                System.out.println("O vencedor é " + this.adversario2.getNome());
-                this.adversario2.ganharLuta();
-                this.adversario1.perderLuta();
-                break;
+
+    public void marcarCombate(Lutador lutador1, Lutador lutador2) {
+        if (lutador1 != lutador2
+                && lutador1.getCategoria().equals(lutador2.getCategoria())) {
+            System.out.println("Combate pode ser marcado");
+            this.setAprovado(true);
+            this.setAdversario1(lutador1);
+            this.setAdversario2(lutador2);
+        } else {
+            this.setAprovado(false);
+            this.adversario1 = null;
+            this.adversario2 = null;
+            System.out.println("Combate nao pode ser marcado");
         }
-        System.out.println("====================================");
     }
-}
+
+    public void Combater() {
+        if (this.getAprovado()) {
+            this.getAdversario1().apresentar();
+            this.getAdversario2().apresentar();
+
+            Random opcao = new Random();
+            int resultado = opcao.nextInt(3);
+
+            System.out.println("========================================");
+
+            switch (resultado) {
+                case 0:
+                    System.out.println("Resultado do combate foi um empate !!");
+                    this.getAdversario1().empatarLuta();
+                    this.getAdversario2().empatarLuta();
+                    break;
+                case 1:
+                    System.out.println("O vencedor é " + this.getAdversario1().getNome());
+                    this.getAdversario1().ganharLuta();
+                    this.getAdversario2().perderLuta();
+                    break;
+                case 2:
+                    System.out.println("O vencedor é " + this.getAdversario2().getNome());
+                    this.getAdversario2().ganharLuta();
+                    this.getAdversario1().perderLuta();
+                    break;
+            }
+            System.out.println("====================================");
+        }
+    }
 
     public Lutador getAdversario1() {
         return adversario1;
